8bed6cc4034728b224710d50933f16992dab89e0
fsmosca <fsmosca@users.noreply.github.com>
2024-03-01 12:08:20 +0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from streamlit import session_state as ss
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+from modules.nav import MenuButtons
+
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+
+st.header('Home page')
+
+authenticator.login()
+
+if ss["authentication_status"]:
+    authenticator.logout()    
+    st.write(f'Welcome *{ss["name"]}*')
+
+elif ss["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif ss["authentication_status"] is None:
+    st.warning('Please enter your username and password')
+
+# We call below code in case of registration, reset password, etc.
+with open('config.yaml', 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)
+
+# Call this late because we show the page navigator depending on who logged in.
+MenuButtons()
