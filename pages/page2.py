ce86f7751484bcf472396b081bcfe085a306ef7c
fsmosca <fsmosca@users.noreply.github.com>
2024-03-10 16:25:17 +0800

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from streamlit import session_state as ss
 from modules.nav import MenuButtons
-from pages.login import get_roles
+from pages.account import get_roles
 
 
 # If user refreshes the page, go to the login page because
 # in there we have the facility to check the login status.
 if 'authentication_status' not in ss:
-    st.switch_page('./pages/login.py')
+    st.switch_page('./pages/account.py')
 
 MenuButtons(get_roles())
 st.header('Page 2')
