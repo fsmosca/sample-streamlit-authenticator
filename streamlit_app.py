6b1c4a4987a7ae1403a81f63da0345712e7ac4cb
Ferdinand Mosca <fsmosca@users.noreply.github.com>
2024-03-11 22:14:57 +0800

    Modify comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,10 @@ from streamlit import session_state as ss
 from modules.nav import MenuButtons
 from pages.account import get_roles
 
-# If user refreshes the page, go to the login page because
-# in there we have the facility to check the login status.
+# If the user reloads or refreshes the page while still logged in,
+# go to the account page to restore the login status. Note reloading
+# the page changes the session id and previous state values are lost.
+# What we are doing is only to relogin the user.
 if 'authentication_status' not in ss:
     st.switch_page('./pages/account.py')
 
