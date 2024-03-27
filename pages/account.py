ef38e74cf5b5beeee4fdccb68ef99685fc917f10
fsmosca <fsmosca@users.noreply.github.com>
2024-03-27 15:32:55 +0800

    Remove role in the registration

--- pages/account.py
+++ pages/account.py
@@ -8,29 +8,9 @@ from modules.nav import MenuButtons
 
 CONFIG_FILENAME = 'config.yaml'
 
-if 'is_register' not in ss:
-    ss.is_register = False
-
 
 with open(CONFIG_FILENAME) as file:
     config = yaml.load(file, Loader=SafeLoader)
-
-
-def add_role(username_to_add_role, new_role='user'):
-    """Append role to the config file.
-
-    In the registration widget, there is a role selectbox. This is added
-    because streamlit-authenticator does not support it.
-    """
-    with open(CONFIG_FILENAME) as file:
-        config = yaml.load(file, Loader=SafeLoader)
-
-    # Check if the necessary keys exist before modifying the config
-    if 'credentials' in config and 'usernames' in config['credentials'] and username_to_add_role in config['credentials']['usernames']:
-        config['credentials']['usernames'][username_to_add_role]['role'] = new_role
-
-        with open(CONFIG_FILENAME, 'w') as file:
-            yaml.dump(config, file, default_flow_style=False)
 
 
 def get_roles():
@@ -73,24 +53,16 @@ with login_tab:
 
 with register_tab:
     if not ss["authentication_status"]:
-
-        # Adds custom role.
-        st.selectbox(':green[Role]', options=['user', 'admin'], key='role')
         try:
-            email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(preauthorization=False)
+            email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
             if email_of_registered_user:
                 st.success('User registered successfully')
-                ss.is_register = True
         except Exception as e:
             st.error(e)
 
 # We call below code in case of registration, reset password, etc.
 with open(CONFIG_FILENAME, 'w') as file:
     yaml.dump(config, file, default_flow_style=False)
-
-if ss.is_register:
-    ss.is_register = False
-    add_role(username_of_registered_user, ss.role)
 
 # Call this late because we show the page navigator depending on who logged in.
 MenuButtons(get_roles())
